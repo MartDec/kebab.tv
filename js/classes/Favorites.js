3f4f2a5463a9b0b5b915fbd00db6ecf9dd2d6831
Martin Déclert <declertmartin@gmail.com>
2020-03-27 22:28:51 +0100

    Squashed commit of the following:
    commit a26f4c516463d3c3d2bbdfc5d66d6f6a9bc54512
    Author: Martin Déclert <declertmartin@gmail.com>
    Date:   Fri Mar 27 22:27:56 2020 +0100
    
        Finalisation du module des films favoris
    
    commit 1beef4b118a686c71325c4763b136101718d6421
    Author: Martin Déclert <declertmartin@gmail.com>
    Date:   Fri Mar 27 20:48:03 2020 +0100
    
        création de la classe qui rassemble tous les favoris

---
+++ js/classes/Favorites.js
@@ -0,0 +1,74 @@
+import { Toast } from "./Toast.js";
+import { Favorite } from "./Favorite.js";
+import { FetchRequest } from './FetchRequest.js';
+
+export class Favorites {
+    constructor() {
+        this.wrapper = document.querySelector('#favorite-wrapper');
+        this.api = 'https://kebabtv.dwsapp.io/api';
+        this.favorites = null;
+    };
+
+    async init() {
+        await this.getFavorites();
+        return this;
+    }
+
+    async getFavorites() {
+        let response = await new FetchRequest(
+            `${this.api}/me`,
+            'POST',
+            `{"token": "${localStorage.getItem('user_token')}"}`
+        ).fetch();
+
+        if (response.err === null || typeof response.err === 'undefined') {
+            this.favorites = response.data.favorite;
+        } else {
+            new Toast('error', response.err).show();
+        }
+    };
+
+    display() {
+        document.querySelector('#movies-wrapper').innerHTML = '';
+        this.wrapper.innerHTML = '';
+        for (let favoriteData of this.favorites) {
+            let favorite = new Favorite(favoriteData, this);
+            this.wrapper.appendChild(favorite.element);
+        }
+    };
+
+    async add(movieId, movieTitle) {
+        const params = {
+            id: movieId,
+            title: movieTitle,
+            token: localStorage.getItem('user_token')
+        };
+        let response = await new FetchRequest(
+            `${this.api}/favorite`,
+            'POST',
+            JSON.stringify(params)
+        ).fetch();
+
+        if (response.err === null || typeof response.err === 'undefined') {
+            this.getFavorites();
+            new Toast('success', `${movieTitle} successfully added to favorites`).show();
+        } else {
+            new Toast('error', response.err).show();
+        }
+    };
+
+    async remove(favoriteId, movieTitle) {
+        let response = await new FetchRequest(
+            `${this.api}/favorite/${favoriteId}`,
+            'DELETE',
+            `{"token": "${localStorage.getItem('user_token')}"}`
+        ).fetch();
+
+        if (response.err === null || typeof response.err === 'undefined') {
+            await this.getFavorites();
+            new Toast('success', `${movieTitle} successfully removed from favorites`).show();
+        } else {
+            new Toast('error', response.err).show();
+        }
+    }
+}
