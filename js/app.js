b334f55244a6bc857db5da1cd887ed5ec543d05a
Martin Déclert <declertmartin@gmail.com>
2020-03-28 00:31:37 +0100

    Suppression d'éléments inutile

--- js/app.js
+++ js/app.js
@@ -7,12 +7,11 @@ import { Favorites } from './modules/favorite/Favorites.js';
 document.addEventListener('DOMContentLoaded', () => {
 
     const USER_API = 'https://kebabtv.dwsapp.io/api/';
-    const MOVIE_API = 'https://api.themoviedb.org/3/search/movie?api_key=6fd32a8aef5f85cabc50cbec6a47f92f';
 
     const login = new UserForm('#login-form form', USER_API + 'login');
     const register = new UserForm('#register-form form', USER_API + 'register');
-    const search = new Form('#search-form', MOVIE_API);
 
+    const search = document.querySelector('#search-form');
     const favorite = document.querySelector('#favorite-link');
     const logout = document.querySelector('#logout-link');
 
@@ -30,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
         session.create(userData.data);
     });
 
-    search.element.addEventListener('submit', async e => {
+    search.addEventListener('submit', async e => {
         e.preventDefault();
         const keyword = e.target.querySelector('input').value;
         const list = await new MovieList().search(keyword);
