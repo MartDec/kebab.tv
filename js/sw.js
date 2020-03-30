451f31658099cdbe242c29009478012dad362ec8
Martin Déclert <declertmartin@gmail.com>
2020-03-30 22:42:49 +0200

    Ajout du service worker pour PWA

---
+++ js/sw.js
@@ -0,0 +1,30 @@
+var cacheName = 'kebab.tv';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/skin/css/main.css',
+  '/public'
+];
+
+self.addEventListener('install', e => {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate',  e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
