7ec5e559daf6eefbae2ed173c204f54f3b5aa54f
Martin Déclert <declertmartin@gmail.com>
2020-03-30 23:19:51 +0200

    ajout des icones de l'app et refonte du service worker

--- js/sw.js
+++ js/sw.js
@@ -1,30 +1,43 @@
-var cacheName = 'kebabtv';
-var filesToCache = [
-  '/',
-  '/index.html',
-  '/js/app.js',
-  '/skin/css/main.css',
-  '/public'
+const CACHE_NAME = 'kebab-cache';
+const FILES_TO_CACHE = [
+    '/index.html',
+    '/js/app.js',
+    '/js/modules/*',
+    '/public/img/*',
+    '/skin/css/main.css'
 ];
 
 self.addEventListener('install', e => {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    })
-  );
-});
-
-self.addEventListener('activate',  e => {
-  e.waitUntil(self.clients.claim());
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Cache opened');
+                return cache.addAll(FILES_TO_CACHE);
+            })
+    );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request, {ignoreSearch:true}).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+    e.respondWith(
+        caches.match(e.request)
+        .then(response => {
+            if (response)
+                return response;
+
+            return fetch(e.request)
+                .then(res => {
+                    if(!res || res.status !== 200 || res.type !== 'basic')
+                        return res;
+
+                    const resToCache = res.clone();
+                    caches.open(CACHE_NAME)
+                        .then(cache => {
+                            cache.put(e.request, resToCache);
+                        });
+
+                    return res;
+                });
+        })
+    );
 });
+  
